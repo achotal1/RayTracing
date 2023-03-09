9eb8cc83944d19a66b406306e3febf60cea4de22
achotal1 <achotal1@binghamton.edu>
2023-03-09 09:30:50 -0500

    Antialiasing and add difuse material

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,28 @@ mod vec;
 mod ray;
 mod hit;
 mod sphere;
+mod camera;
 
 use std::io::{stderr, Write};
 use vec::{Vec3, Point3, Color};
 use ray::Ray;
 use hit::{Hit, World};
 use sphere::Sphere;
+use camera::Camera;
+use rand::prelude::*;
 
 
 
-fn ray_color(r: &Ray, world: &World) -> Color{
+fn ray_color(r: &Ray, world: &World, depth: u64) -> Color{
 	// if ray hits sphere
-	if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
-        0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0))
+	if depth <= 0{
+		return Color::new(0.0, 0.0, 0.0);
+	}
+
+	if let Some(rec) = world.hit(r, 0.001, f64::INFINITY) {
+		let target = rec.p + rec.normal + Vec3::random_in_unit_sphere().normalized();
+		let r = Ray::new(rec.p, target - rec.p);
+        0.5 * ray_color(&r, world, depth - 1)
     } else {
         let unit_direction = r.direction().normalized();
         let t = 0.5 * (unit_direction.y() + 1.0);
@@ -27,37 +36,38 @@ fn main() {
 	const ASPECT_RATIO: f64 = 16.0/9.0;
     const IMG_HEIGHT: u64 = ((256 as f64)/ASPECT_RATIO) as u64;
     const IMG_WIDTH : u64 = 256;
+    const  SAMPLES_PER_PIXEL: u64 = 100;
+    const MAX_DEPTH: u64 = 5;
     // world
     let mut world = World::new();
     world.push(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
     world.push(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
     //making the camera
-    let viewport_height = 2.0;
-    let viewport_width = ASPECT_RATIO * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0
-    						- Vec3::new(0.0, 0.0, focal_length);
+    let cam = Camera::new();
 
 
     println!("P3");
     println!("{} {}", IMG_WIDTH, IMG_HEIGHT);
     println!("255");
+    let mut rng = rand::thread_rng();
+
     for j in (0..IMG_HEIGHT).rev(){
     	eprintln!("Scanlines remaining: {:3}", IMG_HEIGHT - j - 1);
     	stderr().flush().unwrap();
+
     	for i in 0..IMG_WIDTH{
-    		let u = (i as f64)/((IMG_WIDTH - 1) as f64);
-    		let v = (j as f64)/((IMG_HEIGHT - 1) as f64);
+    		let mut pixel_color = Color::new(0.0,0.0,0.0);
+    		for _ in 0..SAMPLES_PER_PIXEL{
+    			let random_u: f64 = rng.gen();
+    			let random_v: f64 = rng.gen();
+    			let u = ((i as f64) + random_u)/((IMG_WIDTH - 1) as f64);
+    			let v = ((j as f64) + random_v)/((IMG_HEIGHT - 1) as f64);
+    			let r = cam.get_ray(u,v);
+    			pixel_color += ray_color(&r, &world, MAX_DEPTH);
+    		}
 
-    		let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-    		let pixel_color = ray_color(&r, &world);
-
-            println!("{}", pixel_color.format_color());
+            println!("{}", pixel_color.format_color(SAMPLES_PER_PIXEL));
 
     	}
     }
--- src/vec.rs
+++ src/vec.rs
@@ -1,7 +1,8 @@
 use std::ops::{Index, IndexMut, Add, AddAssign, Sub, SubAssign, Mul, MulAssign, Div, DivAssign};
 use std::fmt;
 use std::fmt::Display;
-//use rand::prelude::*;
+use rand::prelude::*;
+use std::ops::Range;
 
 #[derive(Clone, Copy)]
 pub struct Vec3{
@@ -49,11 +50,31 @@ impl Vec3{
         self / self.length()
     }
 
-    pub fn format_color(self) -> String {
-    format!("{} {} {}", (255.999 * self[0]) as u64,
-                        (255.999 * self[1]) as u64,
-                        (255.999 * self[2]) as u64)
-     
+    pub fn format_color(self, samples_per_pixel: u64) -> String {
+        let ir = (256.0 * (self[0] / (samples_per_pixel as f64)).sqrt().clamp(0.0, 0.999)) as u64;
+        let ig = (256.0 * (self[1] / (samples_per_pixel as f64)).sqrt().clamp(0.0, 0.999)) as u64;
+        let ib = (256.0 * (self[2] / (samples_per_pixel as f64)).sqrt().clamp(0.0, 0.999)) as u64;
+
+        format!("{} {} {}", ir, ig, ib)
+    }
+
+    pub fn random(r: Range<f64>)-> Vec3{
+        let mut rng = rand::thread_rng();
+
+        Vec3{
+            e: [rng.gen_range(r.clone()),
+                rng.gen_range(r.clone()),
+                rng.gen_range(r.clone())]
+        }
+    }
+
+    pub fn random_in_unit_sphere() -> Vec3{
+        loop{
+            let v = Vec3::random(-1.0..1.0);
+            if v.length() < 1.0{
+                return v;
+            }
+        }
     }
 
 }
